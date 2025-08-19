c531ef79a398ae30354ccb31965747953c6ef54c
Ibrahimbaig1997 <ibrahimbaig12345@gmail.com>
2025-08-19 13:23:37 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,15 +11,16 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "MigrationRG"
-    storage_account_name = "migstracc"
-    container_name       = "terraform-learning-yt"
-    key                  = "Global-Static-Web.tfstate"
+    resource_group_name  = "YOUR_RESOURCE_GROUP"
+    storage_account_name = "STORAGE ACCOUNT"
+    container_name       = "CONTAINER"
+    key                  = "FILENAME.tfstate"
 
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "5ade8a7e-f940-457e-9b4e-65bd6e1d0a29"
+  subscription_id = "YOUR SUB_ID"
 }
+
